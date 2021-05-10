274938b65500e1c58df84c57e76c69d80681ddce
William Long <will@williamlong.xyz>
2021-05-10 05:31:33 +0000

    Implemented basic check in src/art/draw.py to make sure each pixel is a valid byte.

--- src/art/draw.py
+++ src/art/draw.py
@@ -1,6 +1,17 @@
 print(__name__)
 
+hex_bytes = ['0x00']
+
 def str_to_hex(string): return hex(int(string, 16))
+
+def test_byte(byte):
+    if int(byte, 16) >= 0:
+        if int(byte, 16) <= 255:
+            print("Good")
+        if int(byte, 16) > 255:
+            print("Too high.")
+    if int(byte, 16) < 0:
+        print("Too low")
 
 canvas = [
     ['0xff', '0xff', '0xff', '0xff'],
@@ -15,6 +26,7 @@ while 1:
         for pixel in row:
             pixel = '0x' + input("Pixel is currently: "+ f'{pixel}'+"  ")
             if pixel == '0x': pixel = str_to_hex('0xff')
-            print(pixel)
+            # print(pixel)
+            test_byte(pixel)
         input("Row (end): " + str(row) + "\n\n\n")
     input(canvas)
