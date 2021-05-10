ca5789e6ed7c820bf415d67823f469cd32a95ab5
William Long <will@williamlong.xyz>
2021-05-10 13:12:20 +0000

    Finished function to test bytes, and will now add functions to read the byte, print the byte, write the byte, and add color.

--- src/art/draw.py
+++ src/art/draw.py
@@ -5,13 +5,17 @@ hex_bytes = ['0x00']
 def str_to_hex(string): return hex(int(string, 16))
 
 def test_byte(byte):
-    if int(byte, 16) >= 0:
-        if int(byte, 16) <= 255:
-            print("Good")
-        if int(byte, 16) > 255:
-            print("Too high.")
-    if int(byte, 16) < 0:
-        print("Too low")
+    while 1:
+        if int(byte, 16) >= 0:
+            if int(byte, 16) <= 255:
+                print("Good")
+                return byte
+            if int(byte, 16) > 255:
+                print("Too high.")
+                byte = '0xff'
+        if int(byte, 16) < 0:
+            print("Too low")
+            byte = '0xff'
 
 canvas = [
     ['0xff', '0xff', '0xff', '0xff'],
@@ -27,6 +31,6 @@ while 1:
             pixel = '0x' + input("Pixel is currently: "+ f'{pixel}'+"  ")
             if pixel == '0x': pixel = str_to_hex('0xff')
             # print(pixel)
-            test_byte(pixel)
+            pixel = test_byte(pixel)
         input("Row (end): " + str(row) + "\n\n\n")
     input(canvas)
