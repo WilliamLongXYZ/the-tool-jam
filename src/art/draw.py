d4f569e9ee38fb7b1f30768d157d9cf3665c80aa
William Long <will@williamlong.xyz>
2021-05-10 05:15:24 +0000

    Changed the way pixels are printed in src/art/draw.py, and might now be ready to add conditional checks.

--- src/art/draw.py
+++ src/art/draw.py
@@ -1,18 +1,20 @@
 print(__name__)
 
+def str_to_hex(string): return hex(int(string, 16))
+
 canvas = [
-    ['ff', 'ff', 'ff', 'ff'],
-    ['ff', 'ff', 'ff', 'ff'],
-    ['ff', 'ff', 'ff', 'ff'],
-    ['ff', 'ff', 'ff', 'ff'],
+    ['0xff', '0xff', '0xff', '0xff'],
+    ['0xff', '0xff', '0xff', '0xff'],
+    ['0xff', '0xff', '0xff', '0xff'],
+    ['0xff', '0xff', '0xff', '0xff'],
 ]
 
 while 1:
     for row in canvas:
-        input("Row (start): " + str(row))
+        input("Row (start): " + f'{row}')
         for pixel in row:
-            pixel = input("Pixel is currently: "+pixel+"  ")
-            if not pixel: pixel = 'ff'
+            pixel = '0x' + input("Pixel is currently: "+ f'{pixel}'+"  ")
+            if pixel == '0x': pixel = str_to_hex('0xff')
             print(pixel)
         input("Row (end): " + str(row) + "\n\n\n")
     input(canvas)
