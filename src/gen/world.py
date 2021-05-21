d87d116f0c83e00cff29e21580ef640f79e374a7
William Long <will@williamlong.xyz>
2021-05-21 15:49:44 +0000

    Slightly changed the way things worked, fixed up src/main.py to be in a while loop, removed lazy code, and began work on list comprehension for src/gen/world.py as well as some other minor changes throughout the refactoring process.

--- src/gen/world.py
+++ src/gen/world.py
@@ -1,35 +1,31 @@
-from gen import randex
+# from gen import randex
+import randex
 
 import random
 
 
 br = '\n\n'
-geographical_feature_dict = {}
-
-
 settlement_information = []
 
 
-def generate_settlement(race_list, feature_list, politics_list):
-    races, diversity = generate_racial_diversity(race_list)
-    local_geography = generate_local_geography(feature_list)
+def generate_settlement(race_list, feature_list, feature_dict, politics_list):
+    # TODO: shops +feature
+    # TODO: people +feature
+    races = generate_racial_diversity(race_list)
+    local_geography = generate_local_geography(feature_list, feature_dict)
     present_politics = generate_political_affiliations(politics_list)
 
 def determine_races(amount, possible):
     races = []
-    for i in range(0, amount):
-        amount-=1
-        race_num = random.randint(0, amount+1)
-        if race_num >= len(possible): race_num = len(possible)-1
-        race = possible[race_num]
-        possible.remove(race)
-        races.append(race)
+    for i in range(amount):
+        race_num = random.randint(0, amount+1)                      # Choose a random number from the list
+        if race_num >= len(possible): race_num = len(possible)-1    # Make sure the number is not too large
+        race = possible[race_num]                                   # Turn the number into a race
+        possible.remove(race)                                       # Prevent repeats
+        races.append(race)                                          # Add race to new list
     return races
 
-def generate_local_geography(feature_list, feature_dict = geographical_feature_dict):
-    # TODO: shops +feature
-    # TODO: people +feature
-    # TODO: create function for users to add their own geographical features +feature
+def generate_local_geography(feature_list, feature_dict):
     for feature in feature_list:
         feature_locations = {"Inside": False, "North": False, "East": False, "South": False, "West": False, }
         feature_present = randex.randbool()
@@ -39,25 +35,21 @@ def generate_local_geography(feature_list, feature_dict = geographical_feature_d
             for location, value in feature_dict[feature].items():
                 feature_dict[feature][location] = randex.randbool()
 
-    settlement_information.append(geographical_feature_dict)
-    return geographical_feature_dict
+    return feature_dict
 
 def generate_racial_diversity(race_list):
-    races_present_amnt = random.randint(1, 3)
-    races = race_list.copy()
-    races_present = determine_races(races_present_amnt, races)
+    race_list = race_list.copy()
+    races_present = determine_races(random.randint(1, len(race_list)), race_list)
     total_percentage = 100
     racial_diversities = []
-    for i in range(0, len(races_present)):
+    for i in range(len(races_present)):
         if i+1 >= len(races_present):
             racial_diversity = total_percentage
-        if i+1 < len(races_present):
+        else:
             racial_diversity = random.randint(1, total_percentage)
         racial_diversities.append(racial_diversity)
         if total_percentage > racial_diversity: total_percentage -= racial_diversity 
 
-    settlement_information.append(races_present)
-    settlement_information.append(racial_diversities)
     return races_present, racial_diversities
 
 def generate_political_affiliations(politic_types):
@@ -66,7 +58,7 @@ def generate_political_affiliations(politic_types):
     affiliations_present = politic_types.copy()
     total_percentage = 100
     affiliation_percentages = []
-    for i in range(0, len(affiliations_present)):
+    for i in range(len(affiliations_present)):
         if i+1 >= len(affiliations_present):
             affiliation_percentage = total_percentage
         if i+1 < len(affiliations_present):
@@ -74,29 +66,26 @@ def generate_political_affiliations(politic_types):
         affiliation_percentages.append(affiliation_percentage)
         if total_percentage > affiliation_percentage: total_percentage -= affiliation_percentage 
 
-    settlement_information.append(affiliation_percentages)
     return affiliation_percentages
 
 def print_settlement():
     for i in settlement_information:
-        print(i)
-    print(br)
+        print(i,'\n')
 
 def main():
     geographical_features = {}
+    geographical_feature_dict = {}
     geographical_feature_locations = {}
 
     races = input("What races would you like to have the possiblity of being included in your settlement? Seperate each with a comma.   ")
     race_list = races.split(',')
-    
+    input(race_list)
     print(br)
     
     politics = input("What forms of political affiliations would you like to have the possiblity of being included in your settlement? Seperate each with a comma.  ")
     political_affiliations = politics.split(',')
     
-    
     print(br)
-
 
     geography = input("What features would you like to have the possiblility of being included in the geography around your settlement? Seperate each with a comma.     ")
     geographical_feature_list = geography.split(',')
@@ -108,8 +97,28 @@ def main():
     ammount = int(input("How many settlements would you like to generate? Enter an integer greater than or equal to one.    "))
     print(br)
     global settlement_information
-    for x in range(0, ammount):
-        generate_settlement(race_list, geographical_feature_list, political_affiliations)
+    for x in range(ammount):
+        input(race_list)
+        generate_settlement(race_list, geographical_feature_list, geographical_feature_dict, political_affiliations)
         print_settlement()
         settlement_information = []
-    input("Press the enter key to close.")
+    input("Press ENTER to continue.")
+
+# while True: generate_racial_diversity(["human", "dragon", "elf", "demon", "dwarf", "angle"])
+# main()
+
+test = ['lorem', 'ipsum', 'dolor']
+amount = 3
+number = random.randint(0, 2)
+l = [i for i in test if i != test[number]]
+print(l)
+
+'''
+races = []
+for i in range(amount):                                      # HAVE
+    race_num = random.randint(0, amount+1)                   # NEED          # Choose a random number from the list
+    if race_num >= len(possible): race_num = len(possible)-1 # NEED          # Make sure the number is not too large
+    race = possible[race_num]                                # NEED          # Turn the number into a race
+    possible.remove(race)                                    # NEED          # Prevent repeats
+    races.append(race)                                       # NEED
+'''
